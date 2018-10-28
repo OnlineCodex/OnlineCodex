af007aa7f5316874babb4741387c9d8ad1d29bb1
ZorgVomGork <walter_20@web.de>
2018-10-28 11:24:58 +0100

    Orkunit files added

---
+++ src/main/java/oc/wh40k/units/or/ORDeffkillaWartrike.java
@@ -0,0 +1,39 @@
+package oc.wh40k.units.or;
+
+import oc.Eintrag;
+import oc.OptionsEinzelUpgrade;
+import oc.RuestkammerStarter;
+
+public class ORDeffkillaWartrike extends Eintrag {
+
+    OptionsEinzelUpgrade attacksquig;
+    RuestkammerStarter waffen;
+    
+    boolean megaBool=false;
+    boolean gazzBool=false;
+
+    public ORDeffkillaWartrike() {
+        name = "Warboss on Warbike";
+        grundkosten = getPts("Warboss on Warbike");
+        power = 5;
+
+        add(ico = new oc.Picture("oc/wh40k/images/Waaghboss.gif"));
+
+        add(attacksquig = new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Attack squig", getPts("Attack squig")));
+        
+        seperator();
+        
+        waffen = new RuestkammerStarter(ID, randAbstand, cnt, "ORWaffenUndGeschenke", "");
+        ((ORWaffenUndGeschenke)waffen.getKammer()).setDefaultFK("no weapon");
+        ((ORWaffenUndGeschenke)waffen.getKammer()).setDefaultNK("Big choppa");
+        waffen.initKammer(true,true,false,false,true);
+        waffen.setButtonText("Waffen und Geschenke");
+        add(waffen);
+        waffen.setAbwaehlbar(false);
+        
+        complete();
+    }
+
+    @Override
+    public void refreshen() {}
+}
---
+++ src/main/java/oc/wh40k/units/or/ORGunwagon.java
@@ -0,0 +1,52 @@
+package oc.wh40k.units.or;
+
+import oc.Eintrag;
+import oc.OptionsEinzelUpgrade;
+import oc.OptionsGruppeEintrag;
+import oc.OptionsUpgradeGruppe;
+import oc.OptionsZaehlerGruppe;
+
+public class ORGunwagon extends Eintrag {
+
+	OptionsEinzelUpgrade Panzakanonä;
+	OptionsUpgradeGruppe FK2;
+	OptionsZaehlerGruppe KampfpanzaFK;
+
+	public ORGunwagon() {
+
+		name = "Battlewagon";
+		grundkosten = getPts("Battlewagon");
+		power = 11;
+
+		add(ico = new oc.Picture("oc/wh40k/images/Kampfpanza.gif"));
+
+
+		add(Panzakanonä = new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Killkannon", getPts("Killkannon")));
+		
+		seperator();
+		
+		ogE.addElement(new OptionsGruppeEintrag("Kannon", getPts("Kannon")));
+		ogE.addElement(new OptionsGruppeEintrag("Lobba", getPts("Lobba")));
+		ogE.addElement(new OptionsGruppeEintrag("Zzap gun", getPts("Zzap gun")));
+		add(FK2 = new OptionsUpgradeGruppe(ID, randAbstand, cnt, "", ogE));
+
+		seperator();
+
+		ogE.addElement(new OptionsGruppeEintrag("Big shoota", getPts("Big shoota")));
+		ogE.addElement(new OptionsGruppeEintrag("Rokkit launcha", getPts("Rokkit launcha")));
+		add(KampfpanzaFK = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 4));
+
+		seperator();
+
+		add(new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Deff rolla", getPts("Deff rolla")));
+		add(new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "'Ard Case", getPts("'Ard Case")));
+		add(new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Grabbin' klaw", getPts("Grabbin' klaw")));
+		add(new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Wreckin' ball", getPts("Wreckin' ball")));
+
+		complete();
+	}
+
+	//@OVERRIDE
+	public void refreshen() {
+	}
+}
