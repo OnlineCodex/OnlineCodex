3e5fe2ba93e801722324c042c41ddef05e92ce54
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-11-04 20:43:15 +0100

    added version checker and prompt

---
+++ src/main/java/oc/utils/DesktopUtils.java
@@ -0,0 +1,50 @@
+package oc.utils;
+
+import java.awt.Desktop;
+import java.net.URI;
+
+/**
+ * Utilities for interacting with real desktop environments.
+ */
+public final class DesktopUtils {
+    private DesktopUtils() {}
+
+    public static void openLink(String uri) {
+        try {
+            if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI(uri));
+            } else {
+                Runtime.getRuntime()
+                        .exec(toOpenURLCmd(uri))
+                        .waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String[] toOpenURLCmd(String uri){
+        switch (getOs()) {
+            case WIN: return new String[] {"rundll32 url.dll,FileProtocolHandler " + uri};
+            case MAC: return new String[] {"open", uri};
+            case NIX: return new String[] {"xdg-open", uri};
+            default: throw new IllegalStateException("unsupported OS: " + getOs());
+        }
+    }
+
+    public enum OS {
+        WIN, MAC, NIX
+    }
+
+    public static OS getOs() {
+        String osName = System.getProperty("os.name");
+        if (osName.contains("win")) {
+            return OS.WIN;
+        } else if (osName.contains("mac") || osName.contains("darwin")) {
+            return OS.MAC;
+        } else {
+            return OS.NIX;
+        }
+    }
+
+}
