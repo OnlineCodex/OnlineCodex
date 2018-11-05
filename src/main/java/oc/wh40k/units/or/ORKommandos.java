d322a1f1a4a42c0b9d9a8ea03e8ed4a80edd4356
Jan Koch <jan.koch@bh-elektronik.de>
2018-11-05 11:57:53 +0100

    Changed base points for Kommandos to 8. Removed all options from Kommandos.

--- src/main/java/oc/wh40k/units/or/ORKommandos.java
+++ src/main/java/oc/wh40k/units/or/ORKommandos.java
@@ -6,7 +6,7 @@ public class ORKommandos extends Eintrag {
 
     AnzahlPanel kommandoz;
     OptionsZaehlerGruppe kommandozFK;
-    RuestkammerStarter boss;
+    OptionsEinzelUpgrade boss;
 
     public ORKommandos() {
         kategorie = 2;
@@ -17,31 +17,16 @@ public class ORKommandos extends Eintrag {
 
         add(ico = new oc.Picture("oc/wh40k/images/Kommandoz.gif"));
 
-
         seperator();
 
-        ogE.addElement(new OptionsGruppeEintrag("Big shoota", getPts("Big shoota")));
-        ogE.addElement(new OptionsGruppeEintrag("Burna", getPts("Burna")));
-        ogE.addElement(new OptionsGruppeEintrag("Rokkit launcha", getPts("Rokkit launcha")));
-        add(kommandozFK = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 2));
-
-        seperator();
-
-        boss = new RuestkammerStarter(ID, randAbstand, cnt, "ORWaffenUndGeschenke", "Boss Nob");
-        ((ORWaffenUndGeschenke) boss.getKammer()).setDefaultNK("Power klaw");
-        ((ORWaffenUndGeschenke) boss.getKammer()).setDefaultFK("Slugga");
-        boss.initKammer(false, false, false, false, false);
-        boss.setGrundkosten(getPts("Boss Nob"));
-        boss.setUeberschriftTrotzNullKostenAusgeben(true);
-        add(boss);
-        boss.setAbwaehlbar(false);
+        add(boss = new OptionsEinzelUpgrade(ID, randAbstand, cnt, "", "Boss Nob", getPts("Power klaw") + getPts("Slugga") + getPts("Stikkbombs")));
 
         complete();
     }
 
     //@OVERRIDE
     public void refreshen() {
-        boss.setAbwaehlbar(true);
+        boss.setSelected(true);
         if (kommandoz.getModelle() > 10) {
             power = 6;
         } else if (kommandoz.getModelle() > 5) {
