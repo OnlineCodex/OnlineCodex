844f4ea79eaacee167ce3e9c4a1ffcd0e24b1d5d
cooky <cooky@DESKTOP-EPM2FSR.Speedport_W723_V_Typ_A_1_01_018>
2018-11-07 19:40:47 +0100

    Added Kaptin Badrukk to all ork clans
    Moved index HQs to the end of the list.
    Combined clan lists that contain the same units.

--- src/main/java/oc/wh40k/armies/VOLKOrks.java
+++ src/main/java/oc/wh40k/armies/VOLKOrks.java
@@ -156,10 +156,11 @@ public class VOLKOrks extends BuildaVater {
                     "", "Kill Tank [FW]", "Kustom Stompa [FW]", "Gargantuan Squiggoth [FW]",});
         } else if (getFormationType().equals("Goff")) {
             myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
+                    "", "Warboss", "Deffkilla Wartrike",
+                    "", "Big Mek in Mega Armour", "Big Mek with Shokk Attack Gun",
                     "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun",
-                    "", "Ghazghkull Thraka", "Boss Zagstruk",
+                    "", "Kaptin Badrukk", "Ghazghkull Thraka", "Boss Zagstruk",
+                    "", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Big Mek [Index]", "Big Mek on Warbike [Index]",
                     "", "Ork Mek Boss Buzzgob [FW]"});
             myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
                     "", "Boyz", "Gretchin"});
@@ -180,10 +181,11 @@ public class VOLKOrks extends BuildaVater {
                     "", "Stompa"});
         } else if (getFormationType().equals("Blood Axe")) {
             myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
+                    "", "Warboss", "Deffkilla Wartrike",
+                    "", "Big Mek in Mega Armour", "Big Mek with Shokk Attack Gun",
                     "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun",
-                    "", "Boss Snikrot"});
+                    "", "Kaptin Badrukk", "Boss Snikrot",
+                    "", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Big Mek [Index]", "Big Mek on Warbike [Index]"});
             myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
                     "", "Boyz", "Gretchin"});
             myChooserGruppen.get(ELITE).changeComboBoxAuswahlen(new String[]{
@@ -203,9 +205,11 @@ public class VOLKOrks extends BuildaVater {
                     "", "Stompa"});
         } else if (getFormationType().equals("Deathskulls")) {
             myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
-                    "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun"});
+                    "", "Warboss", "Deffkilla Wartrike",
+                    "", "Big Mek in Mega Armour", "Big Mek with Shokk Attack Gun",
+                    "", "Weirdboy", 
+                    "", "Kaptin Badrukk",
+                    "", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Big Mek [Index]", "Big Mek on Warbike [Index]"});
             myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
                     "", "Boyz", "Gretchin"});
             myChooserGruppen.get(ELITE).changeComboBoxAuswahlen(new String[]{
@@ -226,9 +230,11 @@ public class VOLKOrks extends BuildaVater {
                     "", "Stompa"});
         } else if (getFormationType().equals("Evil Sunz")) {
             myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
+                    "", "Warboss", "Deffkilla Wartrike",
+                    "", "Big Mek in Mega Armour", "Big Mek with Shokk Attack Gun",
                     "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun",
+                    "", "Kaptin Badrukk",
+                    "", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Big Mek [Index]", "Big Mek on Warbike [Index]",
                     "", "Zhadsnark da Rippa [FW]"});
             myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
                     "", "Boyz", "Gretchin"});
@@ -247,55 +253,13 @@ public class VOLKOrks extends BuildaVater {
                     "", "Dakkajet", "Burna-Bommer", "Blitza-Bommer", "Wazbom Blastajet"});
             myChooserGruppen.get(LORD_OF_WAR).changeComboBoxAuswahlen(new String[]{
                     "", "Stompa"});
-        } else if (getFormationType().equals("Snakebites")) {
+        } else if (getFormationType().equals("Snakebites") || getFormationType().equals("Bad Moonz") || getFormationType().equals("Freebooterz")) {
             myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
+            		"", "Warboss", "Deffkilla Wartrike",
+                    "", "Big Mek in Mega Armour", "Big Mek with Shokk Attack Gun",
                     "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun"});
-            myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
-                    "", "Boyz", "Gretchin"});
-            myChooserGruppen.get(ELITE).changeComboBoxAuswahlen(new String[]{
-                    "", "Mek", "Painboy", "Big Mek on Warbike [Index]", "Runtherd",
-                    "", "Burna Boyz", "Tankbustas", "Nobz", "Nob with Waaagh Banner", "Nobz on Warbikes", "Meganobz", "Kommandos"});
-            myChooserGruppen.get(FAST_ATTACK).changeComboBoxAuswahlen(new String[]{
-                    "", "Stormboyz", "Deffkoptas", "Warbikers", "Kustom Boosta-Blastas", "Shokkjump Dragstas", "Boomdakka Snazzwagons", "Rukkatrukk Squigbuggys", "Megatrakk Scrapjet", "", "Wartrakks [Index]", "Skorchas [Index]", "Warbuggies [Index]",
-                    "", "Grot Tanks [FW]", "Grot Mega-Tank [FW]"});
-            myChooserGruppen.get(HEAVY_SUPPORT).changeComboBoxAuswahlen(new String[]{
-                    "", "Deff Dreads", "Killa Kans", "Gorkanaut", "Morkanaut", "Battlewagon", "Gunwagon", "Bonebreaka", "Big Gunz [Index]", "Mek Gunz", "Lootas", "Flash Gitz",
-                    "", "Squiggoth [FW]", "Meka-Dread [FW]", "Lifta Wagon [FW]", "Big Trakk [FW]", "Battlewagon with Supa-Kannon [FW]"});
-            myChooserGruppen.get(DEDICATED_TRANSPORT).changeComboBoxAuswahlen(new String[]{
-                    "", "Trukk"});
-            myChooserGruppen.get(FLIER).changeComboBoxAuswahlen(new String[]{
-                    "", "Dakkajet", "Burna-Bommer", "Blitza-Bommer", "Wazbom Blastajet"});
-            myChooserGruppen.get(LORD_OF_WAR).changeComboBoxAuswahlen(new String[]{
-                    "", "Stompa"});
-        } else if (getFormationType().equals("Bad Moonz")) {
-            myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
-                    "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun"});
-            myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
-                    "", "Boyz", "Gretchin"});
-            myChooserGruppen.get(ELITE).changeComboBoxAuswahlen(new String[]{
-                    "", "Mek", "Painboy", "Big Mek on Warbike [Index]", "Runtherd",
-                    "", "Burna Boyz", "Tankbustas", "Nobz", "Nob with Waaagh Banner", "Nobz on Warbikes", "Meganobz", "Kommandos"});
-            myChooserGruppen.get(FAST_ATTACK).changeComboBoxAuswahlen(new String[]{
-                    "", "Stormboyz", "Deffkoptas", "Warbikers", "Kustom Boosta-Blastas", "Shokkjump Dragstas", "Boomdakka Snazzwagons", "Rukkatrukk Squigbuggys", "Megatrakk Scrapjet", "", "Wartrakks [Index]", "Skorchas [Index]", "Warbuggies [Index]",
-                    "", "Grot Tanks [FW]", "Grot Mega-Tank [FW]"});
-            myChooserGruppen.get(HEAVY_SUPPORT).changeComboBoxAuswahlen(new String[]{
-                    "", "Deff Dreads", "Killa Kans", "Gorkanaut", "Morkanaut", "Battlewagon", "Gunwagon", "Bonebreaka", "Big Gunz [Index]", "Mek Gunz", "Lootas", "Flash Gitz",
-                    "", "Squiggoth [FW]", "Meka-Dread [FW]", "Lifta Wagon [FW]", "Big Trakk [FW]", "Battlewagon with Supa-Kannon [FW]"});
-            myChooserGruppen.get(DEDICATED_TRANSPORT).changeComboBoxAuswahlen(new String[]{
-                    "", "Trukk"});
-            myChooserGruppen.get(FLIER).changeComboBoxAuswahlen(new String[]{
-                    "", "Dakkajet", "Burna-Bommer", "Blitza-Bommer", "Wazbom Blastajet"});
-            myChooserGruppen.get(LORD_OF_WAR).changeComboBoxAuswahlen(new String[]{
-                    "", "Stompa"});
-        } else if (getFormationType().equals("Freebooterz")) {
-            myChooserGruppen.get(HQ).changeComboBoxAuswahlen(new String[]{
-                    "", "Warboss", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Deffkilla Wartrike",
-                    "", "Weirdboy",
-                    "", "Big Mek [Index]", "Big Mek in Mega Armour", "Big Mek on Warbike [Index]", "Big Mek with Shokk Attack Gun"});
+                    "", "Kaptin Badrukk",
+                    "", "Warboss in Mega Armour [Index]", "Warboss on Warbike [Index]", "Big Mek [Index]", "Big Mek on Warbike [Index]"});
             myChooserGruppen.get(TROOPS).changeComboBoxAuswahlen(new String[]{
                     "", "Boyz", "Gretchin"});
             myChooserGruppen.get(ELITE).changeComboBoxAuswahlen(new String[]{
