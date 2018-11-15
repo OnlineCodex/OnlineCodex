9d452179e9dd77f277a400410b21fbdf21e04a89
spacecooky <cooky2k@web.de>
2018-11-15 11:14:31 +0100

    Changed deffdread weapon selection (#64)
    Standard weapons and options are now separated groups.
    Selected options automaticly decrease selected standard options.

--- src/main/java/oc/wh40k/units/or/ORDeffDreadKammer.java
+++ src/main/java/oc/wh40k/units/or/ORDeffDreadKammer.java
@@ -6,7 +6,7 @@ import oc.RuestkammerVater;
 
 public class ORDeffDreadKammer extends RuestkammerVater {
 
-    OptionsZaehlerGruppe w1, w2;
+    OptionsZaehlerGruppe w1, w2, w1x, w2x;
 
     public ORDeffDreadKammer() {
         grundkosten = getPts("Deff Dreads");
@@ -17,29 +17,35 @@ public class ORDeffDreadKammer extends RuestkammerVater {
         add(ico = new oc.Picture("oc/wh40k/images/IGLemanRuss.jpg"));
 
         ogE.addElement(new OptionsGruppeEintrag("Big shoota", getPts("Big shoota")));
+        add(w1x = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 2));
         ogE.addElement(new OptionsGruppeEintrag("Rokkit launcha", getPts("Rokkit launcha")));
         ogE.addElement(new OptionsGruppeEintrag("Skorcha", getPts("skorcha")));
         ogE.addElement(new OptionsGruppeEintrag("Kustom mega-blasta", getPts("Kustom mega-blasta")));
         ogE.addElement(new OptionsGruppeEintrag("Dread saw", getPts("dread saw")));
         add(w1 = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 2));
-        w1.setAnzahl(0, 2);
+        w1x.setAnzahl(0, 2);
 
         seperator();
 
         ogE.addElement(new OptionsGruppeEintrag("Dread klaw", getPts("Dread klaw")));
+        add(w2x = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 2));
         ogE.addElement(new OptionsGruppeEintrag("Rokkit launcha", getPts("Rokkit launcha")));
         ogE.addElement(new OptionsGruppeEintrag("Skorcha", getPts("skorcha")));
         ogE.addElement(new OptionsGruppeEintrag("Kustom mega-blasta", getPts("Kustom mega-blasta")));
         ogE.addElement(new OptionsGruppeEintrag("Dread saw", getPts("Dread saw")));
         add(w2 = new OptionsZaehlerGruppe(ID, randAbstand, cnt, "", ogE, 2));
-        w2.setAnzahl(0, 2);
+        w2x.setAnzahl(0, 2);
 
         sizeSetzen();
     }
 
     @Override
     public void refreshen() {
-
+    	w2x.setMaxAnzahl(2-w2.getAnzahl());
+    	w2x.setAnzahl(0,2-w2.getAnzahl());
+    	
+    	w1x.setMaxAnzahl(2-w1.getAnzahl());
+    	w1x.setAnzahl(0,2-w1.getAnzahl());
     }
 
 }
