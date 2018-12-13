d55de6c23021489e13fb3d4444f9c3563e8096a9
Jan Koch <jan.koch@bh-elektronik.de>
2018-12-13 14:45:58 +0100

    Added SM Relics

--- src/main/java/oc/wh40k/units/im/IMChapterAncient.java
+++ src/main/java/oc/wh40k/units/im/IMChapterAncient.java
@@ -1,5 +1,6 @@
 package oc.wh40k.units.im;
 
+import oc.BuildaHQ;
 import oc.Eintrag;
 import oc.RuestkammerStarter;
 
@@ -12,13 +13,18 @@ public class IMChapterAncient extends Eintrag {
         power = 5;
         
         seperator();
+
+        waffenUndReliquien = new RuestkammerStarter(ID, randAbstand, cnt, IMSpaceMarinesRuestkammer.class, "");
+        ((IMSpaceMarinesRuestkammer) waffenUndReliquien.getKammer()).setType("Chapter Ancient");
+        waffenUndReliquien.initKammer();
+        waffenUndReliquien.setButtonText(BuildaHQ.translate("Waffen & Reliquien"));
+        add(waffenUndReliquien);
+        waffenUndReliquien.setAbwaehlbar(false);
+        
+        seperator();
         
         addWarlordTraits("", true);
 
         complete();
-    }
-
-    @Override
-    public void refreshen() {
     }
 }
