64b025a0efb5c954e2be925f9ebd29366016a785
spacecooky <cooky2k@web.de>
2018-12-22 16:22:20 +0100

    Set warlord traits for named characters (#151)
    * Set warlord traits for named characters
    
    * Minor corrections

--- src/main/java/oc/wh40k/units/ty/TYTheRedTerror.java
+++ src/main/java/oc/wh40k/units/ty/TYTheRedTerror.java
@@ -1,21 +1,17 @@
 package oc.wh40k.units.ty;
 
 import oc.Eintrag;
-import oc.RuestkammerStarter;
 
 public class TYTheRedTerror extends Eintrag {
 	
-
     public TYTheRedTerror() {
         name = "The Red Terror";
         grundkosten = getPts("The Red Terror");
         power = 3;
-        add(ico = new oc.Picture("oc/wh40k/images/TYBroodlord.jpg"));
-        complete();
-
-        seperator();
         
-		addWarlordTraits("", true);
+		addWarlordTraits("Heightened Senses", true);
+		
+        complete();
     }
 
     @Override
