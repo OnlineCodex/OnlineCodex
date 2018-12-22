b8f57b286b598f29cebb5edb8b58419dc67a0e29
spacecooky <cooky2k@web.de>
2018-12-22 16:16:27 +0100

    154 astra militarum bugs (#156)
    * Filled name value for Chimera
    
    * Marked Index melee options

--- src/main/java/oc/wh40k/units/im/IMAstraMilitarumMeleeWeapons.java
+++ src/main/java/oc/wh40k/units/im/IMAstraMilitarumMeleeWeapons.java
@@ -1,7 +1,5 @@
 package oc.wh40k.units.im;
 
-
-
 import oc.BuildaVater;
 
 import oc.OptionsGruppeEintrag;
@@ -12,35 +10,15 @@ import oc.RuestkammerElement;
 
 import java.util.Vector;
 
-
-
 public class IMAstraMilitarumMeleeWeapons extends RuestkammerElement {
 
-
-
     static public Vector<OptionsGruppeEintrag> createRK(String defaultWeapon, String defaultWeaponCost, BuildaVater bv) {
-
         Vector<OptionsGruppeEintrag> ogE = new Vector<OptionsGruppeEintrag>();
-
         addDefault(defaultWeapon, defaultWeaponCost, ogE, bv);
-
-        addElement("Power axe", "Power axe (AM)", defaultWeapon, ogE, bv);
-
-        addElement("Power maul", "Power maul (AM)", defaultWeapon, ogE, bv);
-
+        addElement("Power axe [INDEX]", "Power axe (AM)", defaultWeapon, ogE, bv);
+        addElement("Power maul [INDEX]", "Power maul (AM)", defaultWeapon, ogE, bv);
         addElement("Power sword", "Power sword (AM)", defaultWeapon, ogE, bv);
-
         addElement("Power fist", "Power fist (AM)", defaultWeapon, ogE, bv);
-
         return ogE;
-
     }
-
-
-
 }
-
-
-
-
-
