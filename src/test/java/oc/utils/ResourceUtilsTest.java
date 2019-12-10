ba6e513a4935ee9d4e5bab6632367bd92da94fae
ZorgVomGork <walter_20@web.de>
2019-12-10 01:36:56 +0100

    soros new units

--- src/test/java/oc/utils/ResourceUtilsTest.java
+++ src/test/java/oc/utils/ResourceUtilsTest.java
@@ -37,10 +37,10 @@ public class ResourceUtilsTest {
                     .put(sanitizeKey("The Visarch"), 120)
                     .put(sanitizeKey("The Yncarne"), 337)
                     .put(sanitizeKey("Yvraine"), 132)
-                    .put(sanitizeKey("Callidus Assassin"), 85)
-                    .put(sanitizeKey("Culexus Assassin"), 85)
-                    .put(sanitizeKey("Eversor Assassin"), 85)
-                    .put(sanitizeKey("Vindicare Assassin"), 85)
+                    .put(sanitizeKey("Callidus Assassin"), 95)
+                    .put(sanitizeKey("Culexus Assassin"), 95)
+                    .put(sanitizeKey("Eversor Assassin"), 95)
+                    .put(sanitizeKey("Vindicare Assassin"), 95)
                     .build(),
                 points
         );
